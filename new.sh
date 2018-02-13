cab3d40ef1170ca42cfb247760fb8b7bb10bbb3a
Chris Doggett <doggettc@gmail.com>
2018-02-13 17:41:31 -0600

    More inputs

--- new.sh
+++
@@ -1,7 +0,0 @@
-./extract_text.py input/13168.png
-./extract_text.py input/13832.png
-./extract_text.py input/1984.png
-./extract_text.py input/6320.png
-./extract_text.py input/6996.png
-./extract_text.py input/8000.png
-
