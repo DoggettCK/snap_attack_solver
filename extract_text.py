8e16285a4bedb2c104ff005a3fc25bffb2cb197e
Chris Doggett <doggettc@gmail.com>
2018-02-14 16:05:48 -0600

    WIP - Detecting/resizing boards automatically

--- extract_text.py
+++ extract_text.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import scrabulizer
 from PIL import Image
 import pytesseract
 import requests
@@ -12,14 +13,6 @@ import sys
 import json
 
 MATCH_THRESHOLD = 0.6
-
-LETTER_SCORES = {
-        'A': 2, 'B': 5, 'C': 3, 'D': 3, 'E': 1, 'F': 5,
-        'G': 4, 'H': 4, 'I': 2, 'J': 10, 'K': 6, 'L': 3,
-        'M': 4, 'N': 2, 'O': 2, 'P': 4, 'Q': 10, 'R': 2,
-        'S': 2, 'T': 2, 'U': 4, 'V': 6, 'W': 6, 'X': 9,
-        'Y': 5, 'Z': 10
-        }
 
 def filename_without_ext(filename):
     return os.path.splitext(os.path.basename(filename))[0]
@@ -41,8 +34,14 @@ def build_rack_templates():
 
     return {l: cv2.imread(fn, 0) for (l, fn) in letter_images(rack_pattern)}
 
+def build_icon_templates():
+    icon_pattern = 'templates/*.png'
+
+    return {l: cv2.imread(fn, 0) for (l, fn) in letter_images(icon_pattern)}
+
 TEMPLATES = build_templates()
 RACK_TEMPLATES = build_rack_templates()
+ICON_TEMPLATES = build_icon_templates()
 
 def get_board_rects(image):
     y_coords = [136, 217, 297, 377, 458, 538, 618]
@@ -59,6 +58,29 @@ def get_rack_rects(image):
 
     return [image[y:y+rect_size, x:x+rect_size] for x in x_coords]
 
+def get_board_bounds(image):
+    min_x, min_y = float('inf'), float('inf')
+    max_x, max_y = float('-inf'), float('-inf')
+
+    for (text, template) in ICON_TEMPLATES.items():
+        h, w = template.shape
+        res = cv2.matchTemplate(image, template, cv2.TM_CCOEFF_NORMED)
+        locations = np.where(res >= 0.90)
+        potential_matches = list(zip(*locations[::-1]))
+
+        for (x, y) in potential_matches:
+            min_x = min(min_x, x)
+            max_x = max(max_x, x + w)
+            min_y = min(min_y, y)
+            max_y = max(max_y, y + h)
+
+    return (min_x, min_y, max_x, max_y)
+
+def get_sub_image(image, bounds):
+    (min_x, min_y, max_x, max_y) = bounds
+
+    return image[min_y:max_y, min_x:max_x]
+
 def extract_color_images(input_file, output_dir):
     image = cv2.imread(input_file)
 
@@ -73,84 +95,30 @@ def extract_color_images(input_file, output_dir):
 
     pass
 
-def cleanup_image(input_file):
-    image = cv2.imread(input_file)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image = cv2.threshold(image, 0, 255, cv2.THRESH_TOZERO | cv2.THRESH_OTSU)[1]
+def to_grayscale(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    return cv2.threshold(gray, 0, 255, cv2.THRESH_TOZERO | cv2.THRESH_OTSU)[1]
 
-    return image
+def cleanup_original(input_file):
+    color = cv2.imread(input_file)
+    (y, x, _) = color.shape
+    print("{} (original): {}x{} aspect ratio: {}".format(input_file, x, y, y/x))
+    gray = to_grayscale(color)
+
+    bounding_box = get_board_bounds(gray)
+
+    sub_image = get_sub_image(color, bounding_box)
+    (y, x, _) = sub_image.shape
+    print("{} (bounded): {}x{} aspect ratio: {}".format(input_file, x, y, y/x))
+    return cv2.resize(sub_image, (645, 837))
+
+def load_grayscale(input_file):
+    image = cv2.imread(input_file)
+
+    return to_grayscale(image)
 
 def text_from_image(image):
     return pytesseract.image_to_string(Image.fromarray(image), lang='eng', boxes=False, config="-psm 7")
-
-def blank_scrabulizer_board():
-    options = {
-            'dictionary': 4,
-            'opponent_count': 1,
-            'design': '',
-            'sort_by': 0,
-            'tc_': 0,
-            'ts_': 0,
-            'boardWidth': 8,
-            'boardHeight': 7,
-            'bingo1': 0,
-            'bingo2': 0,
-            'bingo3': 0,
-            'bingo4': 0,
-            'bingo5': 0,
-            'bingo6': 0,
-            'bingo7': 35,
-            'bingo8': 50,
-            'rackLength': 7
-            }
-    board = {"s_{0}_{1}".format(x, y): "" for x in range(0, 16) for y in range(0, 16)}
-    bonuses = {"b_{0}_{1}".format(x, y): "" for x in range(0, 16) for y in range(0, 16)}
-    counts = {"tc{}".format(key): 1 for (key, value) in LETTER_SCORES.items()}
-    scores = {"ts{}".format(key): value for (key, value) in LETTER_SCORES.items()}
-
-    # Merge all default dicts into one
-    return { k: v for d in [options, board, bonuses, counts, scores] for k, v in d.items() }
-
-def scrape_scrabulizer(board, rack, bonuses):
-    url = 'https://www.scrabulizer.com/solver/results'
-    headers = {
-            'origin': 'http://www.scrabulizer.com',
-            'accept-language': 'en-US,en;q=0.9',
-            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
-            'x-prototype-version': '1.7',
-            'x-requested-with': 'XMLHttpRequest',
-            'x-js-version': '3',
-            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-            'accept': 'text/javascript, text/html, application/xml, text/xml, */*',
-            'referer': 'http://www.scrabulizer.com/',
-            'authority': 'www.scrabulizer.com',
-            'dnt': '1'
-            }
-
-    base_board = blank_scrabulizer_board()
-    letters = {"s_{0}_{1}".format(x, y): letter for ((x, y), letter) in board.items()}
-    bonuses = {"b_{0}_{1}".format(x, y): bonus for ((x, y), bonus) in bonuses.items()}
-    rack = {'rack': ''.join(rack)}
-
-    payload = {k: v for d in [base_board, letters, bonuses, rack] for k, v in d.items()}
-
-    req = requests.post(url, headers=headers, data=payload)
-
-    words = []
-
-    if req.status_code == 200:
-        pattern = re.compile("moves = ([^;]+);")
-        match = pattern.search(req.text)
-        if match:
-            words_string = match.groups()[0]
-            words_pattern = re.compile("\[\"(\w+)\",\s*\[(\d+),\s*(\d+)\],\s*(\d+),")
-            words = [format_move(move) for move in words_pattern.findall(words_string)]
-        else:
-            print("Unexpected Scrabulizer format. Couldn't find moves list.")
-    else:
-        print("Unknown Scrabulizer response: {} {}".format(req.status_code, req.reason))
-
-    return words
 
 def format_move(move_tuple):
     (word, x, y, direction) = move_tuple
@@ -170,6 +138,7 @@ def guess_letter(image, templates, debug=False):
         if(len(list(zip(*locations[::-1])))) > 0:
             matches.append((text, res[0][0]))
 
+    # TODO: Refactor this now that you understand it better from get_board_bounds
     matches = sorted(matches, key=lambda match: -match[1])
 
     if any(matches):
@@ -183,21 +152,33 @@ def process(input_file, options):
     scrape = options.get('scrape', True)
     debug = options.get('debug', False)
 
-    print("Input file: {}".format(input_file))
-
     pid = os.getpid()
     output_dir = "output/{}_{}".format(pid, filename_without_ext(input_file))
     os.makedirs(output_dir, exist_ok=True)
+    os.makedirs('cleaned_input', exist_ok=True)
     
-    # TODO: Remove
-    print("Extracting color images")
-    extract_color_images(input_file, output_dir)
+    # TODO: Remove once you have all templates
+    # extract_color_images(input_file, output_dir)
 
-    image = cleanup_image(input_file)
+    # Trim original to just include known back/shuffle buttons
+    bounded = cleanup_original(input_file)
+
+    # Write it to cleaned_input dir
+    filebase = filename_without_ext(input_file)
+    cleaned_filename = 'cleaned_input/{}.png'.format(filebase)
+    cv2.imwrite(cleaned_filename, bounded)
+
+    # Load that as our actual input
+    image = load_grayscale(cleaned_filename) 
+    (y, x) = image.shape
+    print("{}: {}x{} aspect ratio: {}".format(cleaned_filename, x, y, y/x))
 
     board = {}
     bonuses = {}
     rack = []
+
+    if True:
+        sys.exit(0)
 
     # TODO: Get rid of tesseract after you've gotten examples of every letter and just use opencv matching
     print("Parsing board...")
@@ -234,7 +215,7 @@ def process(input_file, options):
 
     if scrape:
         print("Querying Scrabulizer...")
-        moves = scrape_scrabulizer(board, rack, bonuses)
+        moves = scrabulizer.scrape_scrabulizer(board, rack, bonuses)
         for move in moves:
             # TODO: Recursively call until rack is empty
             print(move)
---
+++ scrabulizer.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+import requests
+import re
+
+LETTER_SCORES = {
+        'A': 2, 'B': 5, 'C': 3, 'D': 3, 'E': 1, 'F': 5,
+        'G': 4, 'H': 4, 'I': 2, 'J': 10, 'K': 6, 'L': 3,
+        'M': 4, 'N': 2, 'O': 2, 'P': 4, 'Q': 10, 'R': 2,
+        'S': 2, 'T': 2, 'U': 4, 'V': 6, 'W': 6, 'X': 9,
+        'Y': 5, 'Z': 10
+        }
+
+def scrape_scrabulizer(board, rack, bonuses):
+    url = 'https://www.scrabulizer.com/solver/results'
+    headers = {
+            'origin': 'http://www.scrabulizer.com',
+            'accept-language': 'en-US,en;q=0.9',
+            'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+            'x-prototype-version': '1.7',
+            'x-requested-with': 'XMLHttpRequest',
+            'x-js-version': '3',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'accept': 'text/javascript, text/html, application/xml, text/xml, */*',
+            'referer': 'http://www.scrabulizer.com/',
+            'authority': 'www.scrabulizer.com',
+            'dnt': '1'
+            }
+
+    options = {
+            'dictionary': 4,
+            'opponent_count': 1,
+            'design': '',
+            'sort_by': 0,
+            'tc_': 0,
+            'ts_': 0,
+            'boardWidth': 8,
+            'boardHeight': 7,
+            'bingo1': 0,
+            'bingo2': 0,
+            'bingo3': 0,
+            'bingo4': 0,
+            'bingo5': 0,
+            'bingo6': 0,
+            'bingo7': 35,
+            'bingo8': 50,
+            'rackLength': 7
+            }
+    board = {"s_{0}_{1}".format(x, y): "" for x in range(0, 16) for y in range(0, 16)}
+    bonuses = {"b_{0}_{1}".format(x, y): "" for x in range(0, 16) for y in range(0, 16)}
+    counts = {"tc{}".format(key): 1 for (key, value) in LETTER_SCORES.items()}
+    scores = {"ts{}".format(key): value for (key, value) in LETTER_SCORES.items()}
+
+    # Merge all default dicts into one
+    base_board = { k: v for d in [options, board, bonuses, counts, scores] for k, v in d.items() }
+
+    letters = {"s_{0}_{1}".format(x, y): letter for ((x, y), letter) in board.items()}
+    bonuses = {"b_{0}_{1}".format(x, y): bonus for ((x, y), bonus) in bonuses.items()}
+    rack = {'rack': ''.join(rack)}
+
+    payload = {k: v for d in [base_board, letters, bonuses, rack] for k, v in d.items()}
+
+    req = requests.post(url, headers=headers, data=payload)
+
+    words = []
+
+    if req.status_code == 200:
+        pattern = re.compile("moves = ([^;]+);")
+        match = pattern.search(req.text)
+        if match:
+            words_string = match.groups()[0]
+            words_pattern = re.compile("\[\"(\w+)\",\s*\[(\d+),\s*(\d+)\],\s*(\d+),")
+            words = [format_move(move) for move in words_pattern.findall(words_string)]
+        else:
+            print("Unexpected Scrabulizer format. Couldn't find moves list.")
+    else:
+        print("Unknown Scrabulizer response: {} {}".format(req.status_code, req.reason))
+
+    return words
